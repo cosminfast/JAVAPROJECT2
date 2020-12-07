46e9bfbf7e870410695666cd56f7061dc6fe653d
Cosmin <cosmin.ciocan@savethechildren.org>
2020-12-07 20:21:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Scriu ceva!");
+
+        byte myByte = 2+2;
+
+
+        long y= 555555551235L;
+        double z = 121231231;
+        float a = 12312312312.1F;
+
+
+        int sum;
+        int firstNumber = 3;
+        int secondNumber =4;
+        sum = firstNumber + secondNumber;
+        System.out.println("Suma este: " + sum);
+        System.out.println("Suma este: " + (sum + myByte));
+
+        System.out.println(3%2);
+
+    }
+
+}
